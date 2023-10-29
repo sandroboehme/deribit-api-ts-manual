2d7c035b58e620d686c82fe418e8dc0982bcd68d
Sandro Böhme <sandro.boehme@inovex.de>
2023-10-29 16:14:25 +0100

    fixed ts config

--- api.test.ts
+++ api.test.ts
@@ -6,9 +6,9 @@ describe('sum module', () => {
     test('adds 1 + 2 to equal 3', async () => {
         const client = ApiClient.instance;
         const result = await new MarketDataApi().publicGetBookSummaryByInstrumentGet('BTC-10NOV23-40000-C');
-        const bookSummary = result.body.result[0];
+        // const bookSummary = result.body.result[0];
 
-        const bookSummaryObj: BookSummary = ObjectSerializer.deserialize(bookSummary, "BookSummary");
+        // const bookSummaryObj: BookSummary = ObjectSerializer.deserialize(bookSummary, "BookSummary");
         console.log(client);
         // expect(sum(1, 2)).toBe(3);
     });
